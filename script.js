5677a3350a1261e7eb875c847e8eb9bdb9e45ea3
Elina670 <leonardelina1389@gmail.com>
2026-05-07 10:02:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// Den här funktionen körs när någon klickar på knappen "Hur beställer jag?"
+function visaInfo() {
+    alert("Vad kul att du vill köpa naan! \n\nDu kan beställa genom att skicka ett meddelande till mig personligen eller via min Instagram. \n\nBröden bakas färska vid beställning!");
+}
 
